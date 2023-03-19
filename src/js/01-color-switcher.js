48c6c4e531cda1bca4bf4f6045271ba77f8fee20
Mazur Roman <superioritywork@gmail.com>
2023-03-19 11:22:27 +0100

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStartEl.addEventListener('click', event => {
+  const timerId = setInterval(changeFone, 1000);
+  btnStartEl.disabled = true;
+
+  btnStopEl.addEventListener('click', event => {
+    clearInterval(timerId);
+    btnStartEl.disabled = false;
+  });
+});
+
+function changeFone() {
+  bodyEl.style = `background-color:${getRandomHexColor()};`;
+}
